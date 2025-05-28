eba823453136863f02abd92e28e404a77044f9f3
Thaonhi <202511596+Thaonhi25a4041884@users.noreply.github.com>
2025-05-28 10:52:16 +0700

    final update user-profile

---
+++ app/Models/Comment.php
@@ -0,0 +1,55 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Comment extends Model
+{
+    use HasFactory;
+    protected $table = 'comments';
+
+    public $timestamps = false;
+
+    const CREATED_AT = 'date';
+    const UPDATED_AT = null;
+
+    protected $fillable = [
+        'clientId',
+        'content',
+        'date',
+        'like_count',
+        'commentId',
+    ];
+
+    protected $casts = [
+        'date' => 'datetime',
+    ];
+
+    public function client()
+    {
+        return $this->belongsTo(Clients::class, 'clientId');
+    }
+    public function news()
+    {
+        return $this->belongsToMany(News::class, 'comment_news', 'commentId', 'newsId');
+    }
+    public function parent()
+    {
+        return $this->belongsTo(Comment::class, 'commentId');
+    }
+
+    // Quan hệ phản hồi
+//    public function replies()
+//    {
+//        return $this->hasMany(Comment::class, 'commentId');
+//    }
+    public function replies()
+    {
+        return $this->hasMany(Comment::class, 'commentId')->orderBy('date', 'asc');
+    }
+
+
+
+}
--- resources/views/user-profile.blade.php
+++ resources/views/user-profile.blade.php
@@ -391,7 +391,7 @@
                         <p class="text-gray-600 text-xs">Lưu ngày: ${news.date}</p>
                     </div>
                     <div class="flex justify-between items-center p-4 border-t border-gray-200">
-                        <button data-url="" class="news-read-more-btn px-4 py-2 bg-blue-500 text-white text-sm font-semibold rounded-md shadow-sm hover:bg-blue-600 focus:outline-none focus:ring-2 focus:ring-blue-500 focus:ring-offset-2 transition duration-200 flex items-center space-x-2">
+                        <button data-id=${news.id} class="news-read-more-btn px-4 py-2 bg-blue-500 text-white text-sm font-semibold rounded-md shadow-sm hover:bg-blue-600 focus:outline-none focus:ring-2 focus:ring-blue-500 focus:ring-offset-2 transition duration-200 flex items-center space-x-2">
                             <svg class="w-5 h-5" fill="none" stroke="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg">
                                 <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M12 6.253v13m0-13C10.832 5.477 9.246 5 7.5 5S4.168 5.477 3 6.253v13C4.168 18.477 5.754 18 7.5 18s3.332.477 4.5 1.253m0-13C13.168 5.477 14.754 5 16.5 5c1.747 0 3.332.477 4.5 1.253v13C19.832 18.477 18.246 18 16.5 18c-1.747 0-3.332.477-4.5 1.253"></path>
                             </svg>
@@ -918,8 +918,8 @@
             // Nút Tin tức đã lưu
             document.querySelectorAll('.news-read-more-btn').forEach(button => {
                 button.onclick = (e) => {
-                    const url = e.currentTarget.dataset.url;
-                    window.open(url, '_blank');
+                    const id = e.currentTarget.dataset.id;
+                    window.location.href = `/news/${id}`;
                 };
             });
             document.querySelectorAll('.news-delete-btn').forEach(button => {
