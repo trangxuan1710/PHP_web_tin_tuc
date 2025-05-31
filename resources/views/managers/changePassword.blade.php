4d0c69f6e1a95fc00dc1da5fe37d9f8e9dd89781
trangxuan1710 <25A4042243@hvnh.edu.vn>
2025-05-31 12:39:53 +0700

    fix change password

--- resources/views/managers/changePassword.blade.php
+++ resources/views/managers/changePassword.blade.php
@@ -55,9 +55,6 @@
                 <button type="submit" class="bg-blue-600 hover:bg-blue-700 text-white font-bold py-3 px-6 rounded-md transition-colors duration-200 focus:outline-none focus:ring-2 focus:ring-blue-500 focus:ring-offset-2">
                     Lưu thay đổi
                 </button>
-                <button type="button" class="bg-gray-200 hover:bg-gray-300 text-gray-800 font-bold py-3 px-6 rounded-md transition-colors duration-200 focus:outline-none focus:ring-2 focus:ring-gray-400 focus:ring-offset-2">
-                    Hủy
-                </button>
             </div>
         </form>
     </div>
